f80eec709ef82bf47c4de88dcda03c2b92123ee0
okzapradhana <okzamahendra29@gmail.com>
2020-04-17 14:13:22 +0700

    feat(generate): generate Sqlite file and compare performance
    - Create 2 functions:
      - `connection` to connect with database
      - `create_table` to create new Table within database
    
    - Add codes to `generate_file` function with if else at some lines to
      differentiate when write file.
    
    - Compare each file performance by print the debug result by print exec
      time and file size to `benchmark/result.log` directory.

--- solution.py
+++ solution.py
@@ -10,6 +10,7 @@ import json
 import msgpack
 import pickle
 from tinydb import TinyDB
+import sqlite3
 
 class Solution(object):
     def __init__(self):
@@ -25,7 +26,7 @@ class Solution(object):
         self.amount = list(range(10, 1001))
         
         self.start_date = "01/01/2019"
-        self.end_date = "03/01/2019"
+        self.end_date = "31/01/2019"
         self.start_timestamp = time.mktime(
                                 datetime.datetime.strptime(self.start_date, "%d/%m/%Y").timetuple())
         self.end_timestamp = time.mktime(
@@ -43,13 +44,16 @@ class Solution(object):
             file = open(output_path, "wb")
         elif db_type == "tinydb":
             db = TinyDB(output_path)
+        elif db_type == "sqlite":
+            conn = self.connection(output_path)
+            self.create_table(conn)
 
         #batch process, each write 10000 data will empyting the List to clear Memory
         for timestamp in range(int(self.start_timestamp), int(self.end_timestamp+1), step):
             output = []
-            print("Current timestamp: ", timestamp)
+            #print("Current timestamp: ", timestamp)
             for curr_time_step in range(timestamp, timestamp+step):
-                print("Curr timestamp in step: ", curr_time_step)
+                #print("Curr timestamp in step: ", curr_time_step)
                 id_data = uuid.uuid4()
                 device_id_rand = random.choice(self.device_id)
                 username_choice = [user for user, dev_id in self.device_user.items()
@@ -58,6 +62,8 @@ class Solution(object):
                 amount_rand = random.choice(self.amount)
                 if "csv" in output_path:
                     output.append([id_data, device_id_rand, username_choice, lokasi_choice, amount_rand, curr_time_step])
+                elif db_type == "sqlite":
+                    output.append((str(id_data), device_id_rand, username_choice, lokasi_choice, amount_rand, curr_time_step))
                 elif "json" in output_path or "msgpack" in output_path or "pkl" in output_path:
                     data = {
                         "id": str(id_data),
@@ -79,22 +85,50 @@ class Solution(object):
                 pickle.dump(output, file)
             elif db_type == "tinydb":
                 db.insert_multiple(output)
+            elif db_type == "sqlite":
+                cursorObject = conn.cursor()
+                cursorObject.executemany('''INSERT INTO efishery
+                VALUES(?, ?, ?, ?, ?, ?)
+                ''', output)
+                conn.commit()
         file.close()
 
+    def connection(self, output_path):
+        try:
+            conn = sqlite3.connect(output_path)
+            return conn
+        except sqlite3.Error:
+            print("Error sqlite: ", sqlite3.Error)
+    
+    def create_table(self, conn):
+        cursorObject = conn.cursor()
+    
+        cursorObject.execute('''CREATE TABLE IF NOT EXISTS efishery
+        (id TEXT PRIMARY KEY NOT NULL,
+        device_id CHAR(10) NOT NULL,
+        username CHAR(10) NOT NULL,
+        lokasi CHAR(10) NOT NULL,
+        amount INT NOT NULL,
+        timestamp_data TIMESTAMP NOT NULL);''')
+        print("Table created successfully")
+
+        conn.commit()
+
+
 if __name__ == '__main__':
+    s = Solution()
+    print("Write %d files" % (s.end_timestamp - s.start_timestamp))
     setup = "from __main__ import Solution"
-    
-    exec_time = timeit.timeit('Solution().generate_file("output/efishery.csv", 100000)', setup, number=1)
-    print("Execution time when write as csv format: ", exec_time)
-    
-    exec_time = timeit.timeit('Solution().generate_file("output/efishery.json", 100000)', setup, number=1)
-    print("Execution time when write as json format: ", exec_time)
 
-    exec_time = timeit.timeit('Solution().generate_file("output/efishery.msgpack", 100000)', setup, number=1)
-    print("Execution time when write as msgpack format: ", exec_time)
-
-    exec_time = timeit.timeit('Solution().generate_file("output/efishery.pkl", 100000)', setup, number=1)
-    print("Execution time when write as pickle format: ", exec_time)
-
-    exec_time = timeit.timeit('Solution().generate_file("output/efisherytinydb.json", 100000, db_type="tinydb")', setup, number=1)
-    print("Execution time when write as tinydb format: ", exec_time)
+    files = ['efishery.csv', 'efishery.json', 'efishery.msgpack',
+             'efishery.pkl', 'efisherytinydb.json', 'efishery.db']
+    for file in files:
+        if "tinydb" in file:
+            exec_time = timeit.timeit('Solution().generate_file("output/%s", 100000, db_type="tinydb")' % file, setup, number=1)
+        elif "db" in file:
+            exec_time = timeit.timeit('Solution().generate_file("output/%s", 100000, db_type="sqlite")' % file, setup, number=1)
+        else:
+            exec_time = timeit.timeit('Solution().generate_file("output/%s", 100000)' % file, setup, number=1)
+        print("\nExecution time when write %s format: %f seconds" % (file, exec_time))
+        file_size = (os.stat('output/%s' % file).st_size)/1000000
+        print("File size is %d MegaBytes" % file_size)
